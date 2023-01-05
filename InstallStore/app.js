c779cab53c35d65f23327c81ec39286bd5184a5d
zellasoft <95840371+zellasoft@users.noreply.github.com>
2023-01-05 13:31:52 +0100

    Add files via upload

--- InstallStore/app.js
+++ InstallStore/app.js
@@ -10,35 +10,59 @@
                 document.getElementById('ProductDisplayName').innerHTML = 'Zella Store <span class="marker green">Preview</span>';
             else if (branch == 'release')
                 document.getElementById('ProductDisplayName').innerHTML = 'Zella Store';
-            let lem = navigator.userAgent.indexOf("Mobile Safari") != -1;
-            let dnwk = !ZellaSoft.IsSafari();
-            let lea = navigator.platform === "ARM";
-            if (!(ZellaSoft.IsLedge() && lea && lem && dnwk) && document.cookie != 'wpwarn=true') {
-                alert("Please use Microsoft Edge on Windows 10 Mobile for this site to work.");
-                document.cookie = 'wpwarn=true';
-            }/**/
-
+            if (ZellaSoft.IsLedge()) {
+                if (navigator.platform === "ARM") {
+                    let iosinstall = document.getElementById('W10MSideload');
+                    iosinstall.classList.remove('hidden');
+                } else {
+                    /* x86 */
+                }
+            } else if (ZellaSoft.IsiPhoneSafari()) {
+                let iosinstall = document.getElementById('iOSSideload');
+                iosinstall.classList.remove('hidden');
+            } else {
+                ZellaSoft.View.ContentDialog("Please use the native web browser that came preinstalled with your operating system.");
+            }
         },
         "Unload": function () {
 
         },
         "DownloadVCLib140": function () {
-            return ZellaSoft.Launchers.DownloadFromURI('Microsoft.VCLibs.140.00_14.0.30704.0_arm_8wekyb3d8bbwe.appx', 'Microsoft.VCLibs.140.00_14.0.30704.0_arm_8wekyb3d8bbwe.appx');
+            if (navigator.platform === "ARM") {
+                return ZellaSoft.Launchers.DownloadFromURI('Microsoft.VCLibs.140.00_14.0.30704.0_arm_8wekyb3d8bbwe.appx', 'Microsoft.VCLibs.140.00_14.0.30704.0_arm_8wekyb3d8bbwe.appx');
+            } else {
+                ZellaSoft.View.ContentDialog("Your device doesn't support this architecture.");
+            }
         },
         "DownloadMAI": function () {
-            return ZellaSoft.Launchers.DownloadFromURI('Microsoft.DesktopAppInstaller_1.0.20921.0_arm_8wekyb3d8bbwe.appx', 'Microsoft.DesktopAppInstaller_1.0.20921.0_arm_8wekyb3d8bbwe.appx');
+            if (navigator.platform === "ARM") {
+                return ZellaSoft.Launchers.DownloadFromURI('Microsoft.DesktopAppInstaller_1.0.20921.0_arm_8wekyb3d8bbwe.appx', 'Microsoft.DesktopAppInstaller_1.0.20921.0_arm_8wekyb3d8bbwe.appx');
+            } else {
+                ZellaSoft.View.ContentDialog("Your device doesn't support this architecture.");
+            }
         },
         "DownloadCA": function () {
-            return ZellaSoft.Launchers.DownloadFromURI('ZellaSoft_CA.cer', 'ZellaSoft Root CA.cer');
+            let rs = ZellaSoft.Launchers.DownloadFromURI('ZellaSoft_CA.cer', 'ZellaSoft Root CA.cer');
+            if (ZellaSoft.IsiPhoneSafari()) {
+                ZellaSoft.View.ContentDialog("Open the settings app and install the profile.");
+            }
+            return rs;
         },
-        "SideloadAsUWP": function () {
-            /*ZellaSoft.View.Navigate('#troubleshooter');*/
+        "SideloadAsUWP": function (url) {
             var el = document.getElementById('troubleshooter');
             el.scrollIntoView({ behavior: "smooth" });
-            return ZellaSoft.Sideload(location.origin.toString() + location.pathname.toString() + 'ZellaStore.appxbundle');
+            return ZellaSoft.msSideload(url.toString());
+        },
+        "SideloadAsiOS": function (url) {
+            var el = document.getElementById('troubleshooter');
+            el.scrollIntoView({ behavior: "smooth" });
+            return ZellaSoft.iOSSideload(location.origin + location.pathname + url.toString());
         },
         "Guide": function (tag) {
             return ZellaSoft.View.Navigate(location.origin.toString() + '/Guides/' + (tag || ''));
+        },
+        "OnHref": function (url) {
+            return ZellaSoft.View.Navigate(url.toString());
         }
     };
 })
