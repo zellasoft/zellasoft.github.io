341559c17eaa8167e82845098f92bb9301789dbc
zellasoft <95840371+zellasoft@users.noreply.github.com>
2023-05-15 10:16:55 +0200

    Update app.js

--- InstallStore/app.js
+++ InstallStore/app.js
@@ -11,11 +11,13 @@
             else if (branch == 'release')
                 document.getElementById('ProductDisplayName').innerHTML = 'Zella Store';
             if (ZellaSoft.IsLedge()) {
+                ShowLedgeOutDate();
+                let uwpinstall = document.getElementById('W10MSideload');
                 if (navigator.platform === "ARM") {
-                    let iosinstall = document.getElementById('W10MSideload');
-                    iosinstall.classList.remove('hidden');
-                } else {
-                    /* x86 */
+                    uwpinstall.classList.remove('hidden');
+                } else /* x86 */ {
+                    ZellaSoft.View.ContentDialog("It is possible that sideloading will fail due to the wrong architecture of your device.");
+                    uwpinstall.classList.remove('hidden');
                 }
             } else if (ZellaSoft.IsiPadSafari()) {
                 let ipadosinstall = document.getElementById('iPadOSSideload');
@@ -24,25 +26,38 @@
                 let iosinstall = document.getElementById('iOSSideload');
                 iosinstall.classList.remove('hidden');
             } else {
-                ZellaSoft.View.ContentDialog('Platform:' + navigator.platform.toString() + '\n' + navigator.userAgent.toString());
-                /*ZellaSoft.View.ContentDialog("Please use the native web browser that came preinstalled with your operating system.");*/
+                if (ZellaSoft.View.Parameter('debug').toLowerCase() === 'true') {
+                    ZellaSoft.View.ContentDialog('Platform:' + navigator.platform.toString() + '\n' + navigator.userAgent.toString());
+                } else {
+                    ZellaSoft.View.ContentDialog("Please use the native web browser that came preinstalled with your operating system.");
+                }
             }
         },
         "Unload": function () {
-
+            
+        },
+        "ShowLedgeOutDate": function () {
+            let r = /Edge\/([0-9]+)(\.)([0-9]+)\b/;
+            if (r.test(navigator.userAgent)) {
+                if (parseInt(navigator.userAgent.match(r)[3]) < 15063) {
+                    ZellaSoft.View.ContentDialog("Update Windows 10 Mobile to version: 10.0.15063.0 or later.");
+                } else if (parseInt(navigator.userAgent.match(r)[3]) < 15254) {
+                    ZellaSoft.View.ContentDialog("Optional update to Windows 10 Mobile 10.0.15254.603.");
+                }
+            }
         },
         "DownloadVCLib140": function () {
             if (navigator.platform === "ARM") {
                 return ZellaSoft.Launchers.DownloadFromURI('Microsoft.VCLibs.140.00_14.0.30704.0_arm_8wekyb3d8bbwe.appx', 'Microsoft.VCLibs.140.00_14.0.30704.0_arm_8wekyb3d8bbwe.appx');
             } else {
-                ZellaSoft.View.ContentDialog("Your device doesn't support this architecture.");
+                ZellaSoft.View.ContentDialog("We don't have the right architecture for your device.");
             }
         },
         "DownloadMAI": function () {
             if (navigator.platform === "ARM") {
                 return ZellaSoft.Launchers.DownloadFromURI('Microsoft.DesktopAppInstaller_1.0.20921.0_arm_8wekyb3d8bbwe.appx', 'Microsoft.DesktopAppInstaller_1.0.20921.0_arm_8wekyb3d8bbwe.appx');
             } else {
-                ZellaSoft.View.ContentDialog("Your device doesn't support this architecture.");
+                ZellaSoft.View.ContentDialog("We don't have the right architecture for your device.");
             }
         },
         "DownloadCA": function () {
