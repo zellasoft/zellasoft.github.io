01b360a10bb9e3d6d9758c8dc3a7a1854f0977fc
zellasoft <95840371+zellasoft@users.noreply.github.com>
2023-05-15 11:26:17 +0200

    Update app.js

--- InstallStore/app.js
+++ InstallStore/app.js
@@ -40,16 +40,12 @@
         "ShowLedgeOutDate": function () {
             let r = /Edge\/([0-9]+)(\.)([0-9]+)\b/;
             if (r.test(navigator.userAgent)) {
-                alert(parseInt(navigator.userAgent.match(r)[3]));
-                alert(parseInt(navigator.userAgent.match(r)[3]) < parseInt(22000));
                 if (parseInt(navigator.userAgent.match(r)[3]) < 15063) {
                     ZellaSoft.View.ContentDialog("Update Windows 10 Mobile to version 10.0.15063.0 or later.");
                 } else if (parseInt(navigator.userAgent.match(r)[3]) < 15254) {
                     ZellaSoft.View.ContentDialog("Optional update to Windows 10 Mobile 10.0.15254.603.");
                 } else if (parseInt(navigator.userAgent.match(r)[3]) < 22000) {
-                    alert("dial");
                     ZellaSoft.View.ContentDialog("Running Noice OS");
-                    alert("log");
                 }
             }
         },
