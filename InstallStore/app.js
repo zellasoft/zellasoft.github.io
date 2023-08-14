d79b0ebe77e269f4d68e474a3f0bb4b09424791a
zellasoft <95840371+zellasoft@users.noreply.github.com>
2023-08-14 06:12:36 +0200

    Update app.js

--- InstallStore/app.js
+++ InstallStore/app.js
@@ -3,12 +3,16 @@
         "Run": function () {
             let branch = ZellaSoft.View.Parameter('branch') || 'alpha';
             if (branch == 'alpha')
+                document.getElementById('W10MSideload').href = 'ZellaStore_Alpha.appxbundle';
                 document.getElementById('ProductDisplayName').innerHTML = 'Zella Store <span class="marker red">Alpha</span>';
             else if (branch == 'beta')
+                document.getElementById('W10MSideload').href = 'ZellaStore_Beta.appxbundle';
                 document.getElementById('ProductDisplayName').innerHTML = 'Zella Store <span class="marker yellow">Beta</span>';
             else if (branch == 'preview')
+                document.getElementById('W10MSideload').href = 'ZellaStore_Preview.appxbundle';
                 document.getElementById('ProductDisplayName').innerHTML = 'Zella Store <span class="marker green">Preview</span>';
             else if (branch == 'release')
+                document.getElementById('W10MSideload').href = 'ZellaStore_Release.appxbundle';
                 document.getElementById('ProductDisplayName').innerHTML = 'Zella Store';
             let bsupported = false;
             if (ZellaSoft.IsLedge()) {
